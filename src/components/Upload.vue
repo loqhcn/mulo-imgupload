570e6b51ef1ad7493d87636bc18ac1504b5b3c1f
罗戚洪 <474693310@qq.com>
2020-06-05 18:36:39 +0800

    123

---
+++ src/components/Upload.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>上传</div>
+</template>
+
+<script>
+export default {
+    name:'mulo-imgupload',
+    methods: {
+        name() {
+            
+        }
+    },
+}
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ src/views/Upload.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button @click="select">选择图片</button>
+
+    <mulo-imgupload></mulo-imgupload>
+
+  </div>
+</template>
+
+<script>
+import Upload from "./../components/Upload";
+console.log(Upload.name)
+export default {
+  components: {
+    [Upload.name]: Upload
+  },
+  methods: {
+    select() {}
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
